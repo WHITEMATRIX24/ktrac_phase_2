38456db582323fa47ec6ca8e2db40f77d7f81e9d
Jacob Louis <louisjacob8@gmail.com>
2025-05-21 15:05:39 +0530

    updation in sql quries and added data table component

--- frontend/src/app/Reports/Busposition/page.tsx
+++ frontend/src/app/Reports/Busposition/page.tsx
@@ -1,65 +1,70 @@
-
+import { ReportDataTable } from "@/components/report_datatable";
 import { ReportTable } from "@/components/reportTable";
 import { getBusPosition } from "@/lib/sql_query";
 const columns = [
-    {
-        accessorKey: "SL NO",
-        header: "SL No.",
-        id: "SL NO",
-    },
-    {
-        accessorKey: "CLASS OF BUS",
-        header: "Bus Class",
-        id: "CLASS OF BUS",
-    },
-    {
-        accessorKey: "Service",
-        header: "Service",
-        id: "Service",
-    },
-    {
-        accessorKey: "Training\n/ STC",
-        header: "Training / STC", // Cleaned up header name
-        id: "Training",
-    },
-    {
-        accessorKey: "Enroute",
-        header: "Enroute",
-        id: "Enroute",
-    },
-    {
-        accessorKey: "BTC",
-        header: "BTC",
-        id: "BTC",
-    },
-    {
-        accessorKey: "PRIVATE HIRE",
-        header: "Private Hire",
-        id: "PRIVATE HIRE",
-    },
-    {
-        accessorKey: "TOTAL",
-        header: "Total",
-        id: "TOTAL",
-    },
+  {
+    accessorKey: "SL NO",
+    header: "SL No.",
+    id: "SL NO",
+  },
+  {
+    accessorKey: "CLASS OF BUS",
+    header: "Bus Class",
+    id: "CLASS OF BUS",
+  },
+  {
+    accessorKey: "Service",
+    header: "Service",
+    id: "Service",
+  },
+  {
+    accessorKey: "Training\n/ STC",
+    header: "Training / STC",
+    id: "Training",
+  },
+  {
+    accessorKey: "Enroute",
+    header: "Enroute",
+    id: "Enroute",
+  },
+  {
+    accessorKey: "BTC",
+    header: "BTC",
+    id: "BTC",
+  },
+  {
+    accessorKey: "PRIVATE HIRE",
+    header: "Private Hire",
+    id: "PRIVATE HIRE",
+  },
+  {
+    accessorKey: "TOTAL",
+    header: "Total",
+    id: "TOTAL",
+  },
 ];
 function getInitialDates() {
-    const yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
-    return {
-        initialStart: yesterday.toISOString().split('T')[0],
-        initialEnd: new Date(yesterday.getTime() + 86400000).toISOString().split('T')[0]
-    };
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  return {
+    initialStart: yesterday.toISOString().split("T")[0],
+    initialEnd: new Date(yesterday.getTime() + 86400000)
+      .toISOString()
+      .split("T")[0],
+  };
 }
 
 export default async function ReportPage() {
-    const { initialStart, initialEnd } = getInitialDates();
-    const busData = await getBusPosition();
+  const { initialStart, initialEnd } = getInitialDates();
+  const busData = await getBusPosition();
 
-    return (
-        <div className='p-4'>
-
-            <ReportTable data={busData} columns={columns} />
-        </div>
-    );
+  return (
+    <div className="p-4">
+      <ReportDataTable
+        data={busData}
+        columns={columns}
+        searchKey="CLASS OF BUS"
+      />
+    </div>
+  );
 }
--- frontend/src/lib/sql_query.ts
+++ frontend/src/lib/sql_query.ts
@@ -1,6 +1,28 @@
+"use server";
+
 import db from "./sql_lite";
 
 export async function getBusPosition() {
   const stmt = db.prepare("SELECT * FROM bus_position");
+  return stmt.all();
+}
+
+export async function getDockBusses() {
+  const stmt = db.prepare("SELECT * FROM dock_busess");
+  return stmt.all();
+}
+
+export async function getEnrouteBusses() {
+  const stmt = db.prepare("SELECT * FROM enroute_busess");
+  return stmt.all();
+}
+
+export async function getClasswiseDock() {
+  const stmt = db.prepare("SELECT * FROM classwise_dock");
+  return stmt.all();
+}
+
+export async function getUnitwiseBusDeployment() {
+  const stmt = db.prepare("SELECT * FROM unitwise_bus_deployment");
   return stmt.all();
 }
