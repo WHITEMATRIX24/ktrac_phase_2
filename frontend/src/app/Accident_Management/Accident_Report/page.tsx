f0b761cd47c0471544bffb8979526d7d4663b076
anulisba <anulisba@gmail.com>
2025-06-07 19:34:33 +0530

    dummy images added

--- frontend/src/app/Accident_Management/Accident_Report/page.tsx
+++ frontend/src/app/Accident_Management/Accident_Report/page.tsx
@@ -245,7 +245,7 @@ const PrimaryAccidentReport: React.FC = () => {
         const mappedData: AccidentReference = {
             id: selectedData.accedent_ref_no,
             refNo: selectedData.accedent_ref_no,
-            busNo: selectedData.bus_noo,
+            busNo: selectedData.bus_no,
             accidentPlace: selectedData.accidentPlace,
             accidentDate: selectedData.accedent_date,
             policeStation: selectedData.policeStation,
@@ -1109,57 +1109,61 @@ const PrimaryAccidentReport: React.FC = () => {
                                                         {/* Hardcoded 4 images */}
                                                         <div
                                                             className="cursor-pointer"
-                                                            onClick={() => setPreviewImage("https://example.com/vehicle-damage.jpg")}
+                                                            onClick={() => setPreviewImage("https://res.cloudinary.com/dpo91btlc/image/upload/v1749304400/8_xhmi5s.png")}
                                                         >
                                                             <div className="bg-gray-200 border border-gray-300 rounded h-24 flex items-center justify-center">
-                                                                <div className="text-gray-500 text-center">
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                                                    </svg>
-                                                                    <span className="text-xs mt-1 block">Vehicle Damage</span>
+                                                                <div className="text-gray-500 text-center h-full w-full">
+                                                                    <img
+                                                                        src="https://res.cloudinary.com/dpo91btlc/image/upload/v1749304400/8_xhmi5s.png"
+                                                                        alt="accident"
+                                                                        className="h-full w-full object-fit"
+                                                                    />
                                                                 </div>
                                                             </div>
                                                         </div>
 
                                                         <div
                                                             className="cursor-pointer"
-                                                            onClick={() => setPreviewImage("https://example.com/scene.jpg")}
+                                                            onClick={() => setPreviewImage("https://res.cloudinary.com/dpo91btlc/image/upload/v1749303818/Capture_owmfke.png")}
                                                         >
                                                             <div className="bg-gray-200 border border-gray-300 rounded h-24 flex items-center justify-center">
-                                                                <div className="text-gray-500 text-center">
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-                                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
-                                                                    </svg>
-                                                                    <span className="text-xs mt-1 block">Accident Scene</span>
+                                                                <div className="text-gray-500 text-center h-full w-full">
+                                                                    <img
+                                                                        src="https://res.cloudinary.com/dpo91btlc/image/upload/v1749303818/Capture_owmfke.png"
+                                                                        alt="accident"
+                                                                        className="h-full w-full object-fit"
+                                                                    />
+                                                                </div>
+                                                            </div>
+
+                                                        </div>
+
+                                                        <div
+                                                            className="cursor-pointer"
+                                                            onClick={() => setPreviewImage("https://res.cloudinary.com/dpo91btlc/image/upload/v1749304400/8_xhmi5s.png")}
+                                                        >
+                                                            <div className="bg-gray-200 border border-gray-300 rounded h-24 flex items-center justify-center">
+                                                                <div className="text-gray-500 text-center h-full w-full">
+                                                                    <img
+                                                                        src="https://res.cloudinary.com/dpo91btlc/image/upload/v1749304400/8_xhmi5s.png"
+                                                                        alt="accident"
+                                                                        className="h-full w-full object-fit"
+                                                                    />
                                                                 </div>
                                                             </div>
                                                         </div>
 
                                                         <div
                                                             className="cursor-pointer"
-                                                            onClick={() => setPreviewImage("https://example.com/documents.jpg")}
+                                                            onClick={() => setPreviewImage("https://res.cloudinary.com/dpo91btlc/image/upload/v1749304400/8_xhmi5s.png")}
                                                         >
                                                             <div className="bg-gray-200 border border-gray-300 rounded h-24 flex items-center justify-center">
-                                                                <div className="text-gray-500 text-center">
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-                                                                    </svg>
-                                                                    <span className="text-xs mt-1 block">Documents</span>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-
-                                                        <div
-                                                            className="cursor-pointer"
-                                                            onClick={() => setPreviewImage("https://example.com/other-vehicle.jpg")}
-                                                        >
-                                                            <div className="bg-gray-200 border border-gray-300 rounded h-24 flex items-center justify-center">
-                                                                <div className="text-gray-500 text-center">
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
-                                                                    </svg>
-                                                                    <span className="text-xs mt-1 block">Other Vehicle</span>
+                                                                <div className="text-gray-500 text-center h-full w-full">
+                                                                    <img
+                                                                        src="https://res.cloudinary.com/dpo91btlc/image/upload/v1749304400/8_xhmi5s.png"
+                                                                        alt="accident"
+                                                                        className="h-full w-full object-fit"
+                                                                    />
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -1172,13 +1176,13 @@ const PrimaryAccidentReport: React.FC = () => {
                                             {/* Fullscreen Image Modal */}
                                             {previewImage && (
                                                 <div
-                                                    className="fixed inset-0 bg-black bg-opacity-90 z-50 flex justify-center items-center"
+                                                    className="fixed inset-0 bg-black/70 z-50 flex justify-center items-center"
                                                     onClick={() => setPreviewImage(null)}
                                                 >
                                                     <div className="max-w-4xl w-full p-4">
-                                                        <div className="bg-white rounded-lg overflow-hidden">
-                                                            <div className="p-3 bg-gray-100 flex justify-between items-center">
-                                                                <h3 className="font-medium">Image Preview</h3>
+                                                        <div className=" rounded-lg overflow-hidden">
+                                                            <div className="p-3  flex justify-between items-center">
+                                                                <h3 className="font-medium"></h3>
                                                                 <button
                                                                     className="text-gray-500 hover:text-gray-700"
                                                                     onClick={() => setPreviewImage(null)}
@@ -1188,7 +1192,7 @@ const PrimaryAccidentReport: React.FC = () => {
                                                                     </svg>
                                                                 </button>
                                                             </div>
-                                                            <div className="p-4 flex justify-center bg-black">
+                                                            <div className="p-4 flex justify-center ">
                                                                 <img
                                                                     src={previewImage}
                                                                     alt="Preview"
